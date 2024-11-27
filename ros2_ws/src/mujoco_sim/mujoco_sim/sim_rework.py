85aedf4111d920bd9f3fd02d863c4d625e34b7ae
SaturnX77 <saturnvdt@gmail.com>
2024-11-26 16:55:08 -0800

    created copy of original quadroped.xml & changed to relative pathing in sim_rework

--- ros2_ws/src/mujoco_sim/mujoco_sim/sim_rework.py
+++ ros2_ws/src/mujoco_sim/mujoco_sim/sim_rework.py
@@ -19,7 +19,7 @@ button_right = False
 lastx = 0
 lasty = 0
 
-modelPath = '/home/sara/git_projects/BruinBear/ros2_ws/Quadroped-XML/quadroped.xml'
+modelPath = '/home/sara/git_projects/BruinBear/ros2_ws/Quadroped-XML/quadroped_og.xml'
 displayRefreshRate = 120
 
 class ActuatorPositionPub(Node):
