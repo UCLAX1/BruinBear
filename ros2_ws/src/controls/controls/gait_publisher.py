a9e73c38c0d93afc747489b43200bb6b88b035b7
HanF12 <hfang09@g.ucla.edu>
2025-03-08 06:18:32 -0800

    walk in sqaure

--- ros2_ws/src/controls/controls/gait_publisher.py
+++ ros2_ws/src/controls/controls/gait_publisher.py
@@ -27,57 +27,28 @@ def main(args=None):
     global gaitNode
     gaitNode = gaitPublisher()
     gait_origin = ""
-    gait = "rnr"
+    gait = "s"
+    startTime = time.time()
+    gaitNode.pub_gait(gait)
+    time.sleep(3)
+    gaitNode.pub_gait(gait)
+    time.sleep(3)
     while True:
+        curTime = time.time() - startTime
+    
+        if (curTime % 12 < 6):
+            gait = "rnr"
+        else:
+            gait = "f"
+
         if (gait_origin != gait):
             gaitNode.pub_gait(gait)
-            time.sleep(5)
-            #gait_origin = gait
+            gait_origin = gait
+            #time.sleep(3)
+            #gaitNode.pub_gait(gait)
   
 
 # Run the main function
 if __name__ == "__main__":
     main()
     
-
-'''
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
-
-class GaitPublisher(Node):
-    def __init__(self):
-        super().__init__('gait_pub')
-
-        # Publisher for gait control
-        self.publisher_ = self.create_publisher(String, 'gait_control', 10)
-
-        # Store gait state as an instance variable
-        self.gait = "f"  # Initial gait
-        self.prev_gait = ""  # Track previous gait to avoid redundant publishing
-
-        # Timer to periodically publish gait changes
-        timer_period = 1.0
-        self.timer = self.create_timer(timer_period, self.pub_gait)
-
-    def pub_gait(self):
-        """Publishes gait command if it has changed."""
-        #if self.gait != self.prev_gait:
-        msg = String()
-        msg.data = self.gait
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Published gait: {msg.data}')
-        #self.prev_gait = self.gait  # Update previous gait
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = GaitPublisher()
-    node.pub_gait()
-    print("publishing")
-    #rclpy.spin(node)  # Proper ROS spinning
-    # node.destroy_node()
-    # rclpy.shutdown()
-
-if __name__ == "__main__":
-    main()
-    '''
