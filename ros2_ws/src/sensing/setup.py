2e1af737776742c3763fe556dd2ef4bcef170148
SaturnX77 <saturnvdt@gmail.com>
2025-04-05 14:11:11 -0700

    realsense moved to sensing package

--- ros2_ws/src/sensing/setup.py
+++ ros2_ws/src/sensing/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'hands = sensing.handTracking:main',
+            'depth = sensing.realsense:main',
         ],
     },
 )
