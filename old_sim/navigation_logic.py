44a6d265d2fd61ab975d79b1961a4c98d59fd180
SaturnX77 <saturnvdt@gmail.com>
2025-05-07 23:33:10 -0700

    update to navigation logic for narrow corridors + corridors in the xml

--- old_sim/navigation_logic.py
+++ old_sim/navigation_logic.py
@@ -4,10 +4,13 @@ actions = ['left', 'right']
 state = 'none'
 
 def navigate(range1,range2,range3):
+    print(f'range1: {range1} range2: {range2} range3: {range3}')
     global previousAction, actions, state
     action = 'neutral'
     if previousAction == 'back':
        action =  actions[np.random.randint(0,1)]
+    elif range2 < 1 and range3 < 1 and (range1 > 1 or range1 < 0):
+        action = 'fwd'
     elif (range2 < 0.5 and range2 != -1) or (range2 < 1 and range1 < 1 and range2 != -1 and range1 != -1):
         action = 'right'
     elif (range3 < 0.5 and range3 != -1) or (range3 < 1 and range1 < 1 and range3 != -1 and range1 != -1):
