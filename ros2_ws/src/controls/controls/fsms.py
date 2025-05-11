ec58dfed79eea75c86fff19f974deab3d0b9e778
SaturnX77 <saturnvdt@gmail.com>
2025-05-11 15:52:22 -0700

    preliminary implimentation for fsm ping pong

--- ros2_ws/src/controls/controls/fsms.py
+++ ros2_ws/src/controls/controls/fsms.py
@@ -1,3 +1,4 @@
+import numpy as np
 class FSM():
     def __init__(self, logger = None):
         self.logger = logger
@@ -18,10 +19,42 @@ class StraighLine(FSM):
         return "forward"
 
 class PingPong(FSM):
+    previousAction = 'neutral'
+    actions = ['left','right']
+    state = 'none'
     def __init__(self, logger = None):
         super().__init__(logger)
         
     def update(self, curr_time, obstacles):
-        return "forward"
+        range2, range1, range3 = obstacles
+
+        if self.previousAction == 'back':
+            self.action =  self.actions[np.random.randint(0,1)]
+        elif range2 < 1 and range3 < 1 and (range1 > 1 or range1 < 0):
+            self.action = 'fwd'
+        elif (range2 < 0.5 and range2 != -1) or (range2 < 1 and range1 < 1 and range2 != -1 and range1 != -1):
+            self.action = 'right'
+        elif (range3 < 0.5 and range3 != -1) or (range3 < 1 and range1 < 1 and range3 != -1 and range1 != -1):
+            self.action = 'left'
+        elif range1 < 0.3 and range1 > 0:
+            # print('back hit')
+            self.action = 'back'
+        elif range2 < 1 and range3 < 1 and range2 > 0 and range3 > 0:
+            action = 'back'
+        elif range1 > 0.5 and range1 > 0:
+            action = 'fwd'
+        else:
+            if state == 'random':
+                action = self.previousAction
+            else:
+                randInt = np.random.randint(0,1)
+                action = actions[randInt]
+            pass
+            state = 'random'
+
+        previousAction = action
+
+        return action
+        # return "forward"
     
 
