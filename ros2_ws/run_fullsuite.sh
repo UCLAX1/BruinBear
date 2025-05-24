43378c280a3e803d28f62d566dcce1cb23359a88
SaturnX77 <saturnvdt@gmail.com>
2025-05-24 15:58:00 -0700

    updated full launch file with new sensing stuff

--- ros2_ws/run_fullsuite.sh
+++ ros2_ws/run_fullsuite.sh
@@ -1,5 +1,7 @@
 colcon build --symlink-install
 source install/setup.bash
 ros2 launch controls controls.launch.py &
-ros2 launch realsense2_camera rs_launch.py enable_accel:=true enable_gyro:=true unite_imu_method:=2 &
+ros2 launch realsense2_camera rs_launch.py config_file:=realsense_params.yaml &
 ros2 run sensing depth &
+
+wait
